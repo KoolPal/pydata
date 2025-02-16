f209f41bd63e901753cd29b667defee25ec63826
KoolPal <yourkoolpal@gmail.com>
2025-02-16 18:17:01 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,7 +1,5 @@
 import asyncio
 import logging
-import os
-
 import zendriver as zd
 
 logging.basicConfig(level="INFO")
@@ -9,26 +7,8 @@ logger = logging.getLogger(__name__)
 
 async def main():
     try:
-        # Focus only on browser launch settings
-        config = zd.Config(
-            headless=True,
-            sandbox=False,
-            no_sandbox=True,  # Explicitly set no_sandbox
-            browser_executable_path='/usr/bin/google-chrome',
-            browser_args=[
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage'
-            ]
-        )
-        
         logger.info("Starting browser")
-        browser = await zd.Browser.create(config)
-        
-        # Just try to load about:blank first to verify browser works
-        page = await browser.new_page()
-        await page.goto("about:blank")
-        
+        browser = await zd.start(browser_executable_path="/usr/bin/google-chrome")
         logger.info("Browser started successfully")
         await browser.stop()
         
